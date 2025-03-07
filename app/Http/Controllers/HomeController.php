853845ab040ac591078da64d9f35251fc7d63b49
Gustavo Alegre <gustavo.alegre@innovabox.net>
2025-03-07 16:01:10 -0500

    20250307 modelos y controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\JWTService;
 use App\JokeUser;
 
 class HomeController extends Controller
@@ -18,7 +19,13 @@ class HomeController extends Controller
         // Consultar por existencia del usuario
         $joke_user = JokeUser::where('email', $request->input('email'))->where('password', md5($request->input('password')))->first();
         if ($joke_user){
-            return response()->json(['status' => 200, 'message' => 'found'], 200);    
+            $userData =[
+                'sub' => $joke_user->email,
+                'first_name' => $joke_user->first_name,
+                'last_name' => $joke_user->last_name
+            ];
+            $token = app(JWTService::class)->generate($userData);
+            return response()->json(['status' => 200, 'token' => $token], 200);
         } else {
             return response()->json(['status' => 401, 'message' => 'BAD CREDENTIALS'], 401);
         }
