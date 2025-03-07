54c470d3598336e404c1a19955e5448535a87d05
Gustavo Alegre <gustavo.alegre@innovabox.net>
2025-03-07 17:06:21 -0500

    20250307 modelos y controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use App\Services\JWTService;
 use App\JokeUser;
@@ -25,10 +26,37 @@ class HomeController extends Controller
                 'last_name' => $joke_user->last_name
             ];
             $token = app(JWTService::class)->generate($userData);
-            return response()->json(['status' => 200, 'token' => $token], 200);
+
+            return response()->json(['status' => 200, 'token' => "{$token}"], 200);
         } else {
             return response()->json(['status' => 401, 'message' => 'BAD CREDENTIALS'], 401);
         }
         
+    }
+
+    /**
+     * Home.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function home(Request $request){
+        $url = "https://sv443.net/jokeapi/v2/joke/Any";
+        
+        $response = Http::withOptions([
+            'verify' => false,
+        ])->withHeaders([
+            'Accept' => 'application/json',
+        ])->get($url);
+        
+        if ($response->successful()) {
+            $data = $response->json();
+            // Process the data
+            return response($response)->withHeaders([
+                'Content-Type' => 'application/json'
+            ]);
+        } else {
+            // Handle the error
+        }
     }
 }
